b290f1124139b5bb854998c72eb946bcf0c6e155
Clara Hultén <clahul-9@student.ltu.se>
2019-11-11 17:49:38 +0100

    bounse 2

--- bounce/bounce/bounce.cpp
+++ bounce/bounce/bounce.cpp
@@ -6,17 +6,25 @@ int main()
 {
 	int x;
 	cin >> x;
-	for (int i = 0; i < (x*2); i++)
+	if (x<0)
 	{
-		if (x>=0)
+		cout << "Error";
+	}
+	else
+	{
+		int q = (2 * x)+1;
+		for (int i = 0; i < q; i++)
 		{
-			cout <<x;
-			x - 1;
-		}
-		else if (x>0)
-		{
-			cout << -x;
-			x - 1;
+			if (x >= 0)
+			{
+				cout << x << "\n";
+				x = x - 1;
+			}
+			else if (x < 0)
+			{
+				cout << -1 * x << "\n";
+				x = x - 1;
+			}
 		}
 	}
 	char y;
