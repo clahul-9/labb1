69df042f0d0b06d4d9e773bb350b61036fed55a7
Clara Hultén <clahul-9@student.ltu.se>
2019-11-12 08:58:51 +0100

    prim numbers 1

---
+++ primNr/primNr/prime_nr.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <array>
+#include <math.h>
+
+using namespace std;
+
+int main() 
+{
+	int n;
+	cin >> n;
+
+	int prim[] = { 2 };
+	while (size(prim)<=n)
+	{
+		switch (switch_on)
+		{
+		default:
+			break;
+		}
+	}
+}
