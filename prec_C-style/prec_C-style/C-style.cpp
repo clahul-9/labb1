010438c1f2fb2c22b5c70002eda53961d4a34663
Clara Hultén <clahul-9@student.ltu.se>
2019-11-14 14:15:03 +0100

    person record c style

--- prec_C-style/prec_C-style/C-style.cpp
+++ prec_C-style/prec_C-style/C-style.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+#include<stdio.h>
+
+int main() {
+	char name[25];
+	int age;
+	char gender;
+	char address[25];
+	int	maride;
+	float height;
+    //Asking dor info
+	printf_s("Enter <name> <age> <gender(m/f)> <address> <maride(true=1/false=0)> <height in meters> :");
+	//scaning info 
+	scanf_s("%24s %d %c %24s %d %f",name,sizeof(name),&age,&gender,1,&address,sizeof(address),&maride,&height);
+	printf("\n\n\n %s \n %i \n %c \n %s \n %i \n %f", name, age,gender,address,maride, height);
+
+	char u;
+	std::cin >> u;
+}
